82c8dd228cabdf3b69aaf0369eda975c6f9d0687
BrendanR81 <brendan.w.richards-1@ou.edu>
2025-08-27 14:52:48 -0500

    Testing stuff
    plesase?

---
+++ Test.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Test 
+{
+
+	public static void main(String[] args) 
+		{
+			int[] nums = {4, 5, 9, 11, 13, 5, 8, 2, 12, 82};
+			int out = 0;
+			
+			for (int i = 0; i < nums.length; i++)
+			{
+				if (nums[i]%2 == 0)
+				{
+					out+=nums[i];
+				}
+			}
+			System.out.println(out);
+		}
+	}
